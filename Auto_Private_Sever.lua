42cbc0014db23b46e144641db8f09a01616cf167
RBLX-own <noone0000n@gmail.com>
2025-10-04 15:52:56 +0630

    Update Auto_Private_Sever.lua

--- Auto_Private_Sever.lua
+++ Auto_Private_Sever.lua
@@ -417,7 +417,8 @@ serverButton.MouseButton1Click:Connect(function()
         successTween:Play()
         
         serverButton.Text = "SERVER CREATED!"
-        serverStatusLabel.Text = "Private server created successfully!"
+        serverStatusLabel.Text = "Private server created successfully
+ID is copied in your clipboard!"
         serverStatusLabel.TextColor3 = Color3.new(0.5, 1, 0.5)
     else
         -- Error state
